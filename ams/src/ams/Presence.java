b1687b83c1bc92f1d4d43c39a5d844feaa73b40f
TwistedRail <thema3an@gmail.com>
2019-05-08 07:25:29 +0400

    Reworked the whole presence taking method, it's clearer now, but still needs work; // TO-DO 	• Create method to automatically fill the ATTEND_LIST every night and reset the current ATTEND/INSTRUCTOR_ATTEND list to FALSE on all records. 		- 2330hrs sounds like a good time. 	• Honestly, just clean up. Everything's working fine, just not ready to be deployed yet. 	• Make a fancy powerpoint? Honestly I should just hire a public speaker to do the presentation for me. I'd give them the info they need and they'll make it sound cohesive.. Maybe I should do that..

--- ams/src/ams/Presence.java
+++ ams/src/ams/Presence.java
@@ -121,11 +121,8 @@ public class Presence {
     public static boolean readStudentUID = false;
     public static String UIStudentName;
     public static void MarkPresent(String UID){
-        
-        int id_test = -1;
         //TO-DO Main Attendance taking method
         int class_id = ClassThread.classCheck();
-        class_id = 2;
         if(timeCheck()){
             int class_idFromTable; String student_idFromTable;
             System.out.print(" MarkPresent() reached successfully. ");
@@ -143,45 +140,8 @@ public class Presence {
                             "    JOIN ACTIVE_CLASSES ON ACTIVE_CLASSES.ID = ATTEND.CLASS_ID\n" +
                             "    WHERE STUDENT.CARD_ID = '"+UID+"'\n" +
                             "    AND ACTIVE_CLASSES.ID = "+class_id+")");
-         /*   
-            dbUpdate.dbComdUpdate("SELECT * FROM ATTEND\n" +
-                    "    JOIN STUDENT ON STUDENT.ID = ATTEND.STUDENT_ID \n" +
-                    "    JOIN ACTIVE_CLASSES ON ACTIVE_CLASSES.ID = ATTEND.CLASS_ID\n" +
-                    "    WHERE STUDENT.CARD_ID = '"+UID+"'\n" +
-                    "    AND ACTIVE_CLASSES.ID = "+class_id+"");   
-            System.out.println("||  " + dbUpdate.rsUpdate.getConcurrency() + " CONCURRENCY HERE ");
-            System.out.println("||  " + ResultSet.CONCUR_UPDATABLE+ " CONCURRENCY HERE ResultSet.CONCUR_UPDATABLE ");
-            
-                if(dbUpdate.rsUpdate.next()){ //dbControl.rs.updateRow();
-                    //dbUpdate.rs.moveToInsertRow();
-                    student_idFromTable = dbUpdate.rsUpdate.getString("STUDENT_ID");
-                    class_idFromTable = dbUpdate.rsUpdate.getInt("CLASS_ID");
-                    dbUpdate.dbComdUpdate("select present from attend where student_id = "+student_idFromTable+" and card_id= "+class_idFromTable);
-                    dbUpdate.rsUpdate.updateBoolean("present", true);
-                    dbUpdate.rsUpdate.updateRow();
-//                    id_test = db.rs.getInt("ID");
-//System.out.println("ID # is: "+id_test);
-//} db.doClose();
-//dbControl.dbComdUpdate("SELECT * FROM STUDENT_PRESENCE JOIN STUDENT ON STUDENT_PRESENCE.STUDENT_ID = STUDENT.ID WHERE STUDENT_PRESENCE.STUDENT_ID = " +id_test);
-//                if(dbControl.rs.next()){
-//                    k = Integer.parseInt(dbControl.rs.getInt("PRESENT") +"");
-//                    System.out.println("Reached this part! "+dbControl.rs.getRow()+" "+k); //Checking
-//                    k++;
-//                    w = (""+k);
-//                    readStudentUID = true;
-//                    UIStudentName = dbControl.rs.getString("first_name")+" "+dbControl.rs.getString("last_name");
-//                    //dbControl.dbComdUpdate("select present from student_presence");
-//                    dbControl.rs.updateString("present",w);
-//                    dbControl.rs.updateRow();
-//                }
-//dbControl.doClose();
-                }else {
-//Meh, not much to do here  lol
-                }*/
             } catch (SQLException ex) {
                 Logger.getLogger(Presence.class.getName()).log(Level.SEVERE, null, ex);
-            
-//InterfaceCmds.getCurrentStudent(UID);
             }finally {dbUpdate.doClose();}
 
         
@@ -206,61 +166,85 @@ public class Presence {
     }
     public static void UpPresence(String UID){
         //TO-DO Present +1 
-        //int classNum = ClassThread.classCheck();
-        int id_test = -1;
-        //TO-DO Main Attendance taking method
+        int class_id = ClassThread.classCheck();
         if(timeCheck()){
-            int k; String w;
+            int class_idFromTable; String student_idFromTable;
+            System.out.print(" MarkPresent() reached successfully. ");
             try {
-                db.dbComd("SELECT ID FROM STUDENT WHERE CARD_ID ='"+UID+"'");
-                if(db.rs.next()){
-                    id_test = db.rs.getInt("ID");
-                    //System.out.println("ID # is: "+id_test);
-                }
-                db.dbComdUpdate("SELECT * FROM STUDENT_PRESENCE WHERE STUDENT_ID = " +id_test);
-                if(db.rs.next()){
-                    k = Integer.parseInt(db.rs.getInt("PRESENT") +"");
-                    System.out.println("Reached this part! "+db.rs.getRow()+" "+k); //Checking
-                    k++;
-                    w = (""+k);
-                    db.rs.updateString("present",w);
-                    db.rs.updateRow();
-                }
-            }catch (SQLException ex) {
-                Logger.getLogger(ams_main.class.getName()).log(Level.SEVERE, null, ex);
-            }finally{ db.doClose();}
-        }else {
-            //Meh, not much to do here  lol
+            dbUpdate.DO_THE_THING = dbUpdate.doConnect().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            dbUpdate.DO_THE_THING.executeUpdate("update attend \n" +
+                    "    set present = true \n" +
+                    "    where student_id = (SELECT attend.student_id FROM ATTEND\n" +
+                    "    JOIN STUDENT ON STUDENT.ID = ATTEND.STUDENT_ID \n" +
+                    "    JOIN ACTIVE_CLASSES ON ACTIVE_CLASSES.ID = ATTEND.CLASS_ID\n" +
+                    "    WHERE STUDENT.CARD_ID = '"+UID+"'\n" +
+                            "    AND ACTIVE_CLASSES.ID = "+class_id+")\n" +
+                            "    and class_id = (SELECT attend.class_id FROM ATTEND\n" +
+                            "    JOIN STUDENT ON STUDENT.ID = ATTEND.STUDENT_ID \n" +
+                            "    JOIN ACTIVE_CLASSES ON ACTIVE_CLASSES.ID = ATTEND.CLASS_ID\n" +
+                            "    WHERE STUDENT.CARD_ID = '"+UID+"'\n" +
+                            "    AND ACTIVE_CLASSES.ID = "+class_id+")");
+            } catch (SQLException ex) {
+                Logger.getLogger(Presence.class.getName()).log(Level.SEVERE, null, ex);
+            }finally {dbUpdate.doClose();}
+            
         }
     }
     public static void DownPresence(String UID){
         //TO-DO Present -1
-        //int classNum = ClassThread.classCheck();
-        int id_test = -1;
-        //TO-DO Main Attendance taking method
+        int class_id = ClassThread.classCheck();
         if(timeCheck()){
-            int k; String w;
+            int class_idFromTable; String student_idFromTable;
+            System.out.print(" MarkPresent() reached successfully. ");
             try {
-                db.dbComd("SELECT ID FROM STUDENT WHERE CARD_ID ='"+UID+"'");
-                if(db.rs.next()){
-                    id_test = db.rs.getInt("ID");
-                    //System.out.println("ID # is: "+id_test);
-                }
-                db.dbComdUpdate("SELECT * FROM STUDENT_PRESENCE WHERE STUDENT_ID = " +id_test);
-                if(db.rs.next()){
-                    k = Integer.parseInt(db.rs.getInt("PRESENT") +"");
-                    System.out.println("Reached this part! "+db.rs.getRow()+" "+k); //Checking
-                    if (k > 0) k--;
-                    w = (""+k);
-                    db.rs.updateString("present",w);
-                    db.rs.updateRow();
-                }
-            }catch (SQLException ex) {
-                Logger.getLogger(ams_main.class.getName()).log(Level.SEVERE, null, ex);
-            }finally{ db.doClose();}
-        }else {
-            //Meh, not much to do here  lol
+            dbUpdate.DO_THE_THING = dbUpdate.doConnect().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            dbUpdate.DO_THE_THING.executeUpdate("update attend \n" +
+                    "    set present = false \n" +
+                    "    where student_id = (SELECT attend.student_id FROM ATTEND\n" +
+                    "    JOIN STUDENT ON STUDENT.ID = ATTEND.STUDENT_ID \n" +
+                    "    JOIN ACTIVE_CLASSES ON ACTIVE_CLASSES.ID = ATTEND.CLASS_ID\n" +
+                    "    WHERE STUDENT.CARD_ID = '"+UID+"'\n" +
+                    "    AND ACTIVE_CLASSES.ID = "+class_id+")\n" +
+                    "    and class_id = (SELECT attend.class_id FROM ATTEND\n" +
+                    "    JOIN STUDENT ON STUDENT.ID = ATTEND.STUDENT_ID \n" +
+                    "    JOIN ACTIVE_CLASSES ON ACTIVE_CLASSES.ID = ATTEND.CLASS_ID\n" +
+                    "    WHERE STUDENT.CARD_ID = '"+UID+"'\n" +
+                    "    AND ACTIVE_CLASSES.ID = "+class_id+")");
+            } catch (SQLException ex) {
+                Logger.getLogger(Presence.class.getName()).log(Level.SEVERE, null, ex);
+            }finally {dbUpdate.doClose();}
+            
         }
+    }
+    
+    public static void InstructorPresence(String UID){
+        
+        int class_id = ClassThread.classCheck();
+        if(timeCheck()){
+            int class_idFromTable; String student_idFromTable;
+            System.out.print(" MarkPresent() reached successfully. ");
+            try {
+            dbUpdate.DO_THE_THING = dbUpdate.doConnect().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            dbUpdate.DO_THE_THING.executeUpdate("update instructor_attend\n" +
+                    "        set present = true \n" +
+                    "        where instructor_id = (select instructor_id from instructor_attend \n" +
+                    "        join instructor on instructor_attend.instructor_id = instructor.ID\n" +
+                    "        join active_classes on instructor_attend.CLASS_ID = active_classes.CLASS_ID\n" +
+                    "        join class on instructor_attend.CLASS_ID = class.id\n" +
+                    "        where instructor.CARD_ID = '"+UID+"'\n" +
+                    "        and active_classes.class_id =" +class_id+")\n" +
+                    "        and class_id = (select instructor_attend.class_id from instructor_attend \n" +
+                    "        join instructor on instructor_attend.instructor_id = instructor.ID\n" +
+                    "        join active_classes on instructor_attend.CLASS_ID = active_classes.CLASS_ID\n" +
+                    "        join class on instructor_attend.CLASS_ID = class.id\n" +
+                    "        where instructor.CARD_ID = '"+UID+"'\n" +
+                    "        and active_classes.class_id = "+class_id+" );");
+            } catch (SQLException ex) {
+                Logger.getLogger(Presence.class.getName()).log(Level.SEVERE, null, ex);
+            }finally {dbUpdate.doClose();}
+            
+        }
+        
     }
     
     public static void getPresncePercentage(String UID){
